cd3300f82c68f8e57806a25cc17b1ada7644e966
yoggy <yoggy0@gmail.com>
2013-07-18 22:51:18 +0900

    modified:   README.md

--- README.md
+++ README.md
@@ -15,3 +15,12 @@ gdb
   (gdb) set environment LD_PRELOAD=LD_PRELOD=/path/to/hook_fork.so
   (gdb) run
 </pre>
+
+gdbserver
+========
+<pre>
+  $ gdbserver --wrapper env LD_PRELOAD=./hook_fork.so -- :2222 ./test_main
+<pre>
+
+see also:
+  * http://sourceware.org/gdb/current/onlinedocs/gdb/Server.html
--- hook_fork.c
+++ hook_fork.c
@@ -8,6 +8,11 @@
 //      $ gdb ./target_program
 //      (gdb) set environment LD_PRELOAD=LD_PRELOD=/path/to/hook_fork.so
 //      (gdb) run
+//
+//  gdbserver:
+//      $ gdbserver --wrapper env LD_PRELOAD=./hook_fork.so -- :2222 ./test_main
+//
+//      see also: http://sourceware.org/gdb/current/onlinedocs/gdb/Server.html
 // 
 int fork()
 {
